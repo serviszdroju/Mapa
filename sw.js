d23c43109e7f12b55ff15e8fec44c82a18e86b56
Jan Soldan <jansoldan@Jan--MacBook-Air.local>
2026-08-10 20:00:02 +0200

    Deploy performance v163

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "astip-szz-v162";
+const CACHE_VERSION = "astip-szz-v163";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const TILE_CACHE = "astip-szz-map-tiles-v1";
