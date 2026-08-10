90900b8fb9c67861e5862d633035279c9e29ffcb
Jan Soldan <jansoldan@Jan--MacBook-Air.local>
2026-08-10 12:46:27 +0200

    Deploy performance phase 74

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "astip-szz-v120";
+const CACHE_VERSION = "astip-szz-v121";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const TILE_CACHE = "astip-szz-map-tiles-v1";
