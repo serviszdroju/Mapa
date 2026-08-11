8529e4e28abc0e94db6806576e1127f257dc8c30
Jan Soldan <jansoldan@Jan--MacBook-Air.local>
2026-08-11 20:34:59 +0200

    Deploy local state object cache

--- late.js
+++ late.js
@@ -2920,7 +2920,7 @@ function reportSzzServiceWorkerError(err){
 function registerSzzServiceWorker(){
   if(!("serviceWorker" in navigator) || !/^https?:$/.test(location.protocol)) return Promise.resolve(null);
   if(window.__szzServiceWorkerRegistrationPromise) return window.__szzServiceWorkerRegistrationPromise;
-  const serviceWorkerBuildVersion="2026-08-11-offline-detail-meta-cache-v222";
+  const serviceWorkerBuildVersion="2026-08-11-local-state-object-cache-v223";
   const activatedKey=`astipSzzSwActivated:${serviceWorkerBuildVersion}`;
   if(!window.__szzSwControllerChangeBound){
     window.__szzSwControllerChangeBound=true;
