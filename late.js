d3568829426887d6ace53a54e7dbfa6efab8dad1
Jan Soldan <jansoldan@Jan--MacBook-Air.local>
2026-08-11 17:18:28 +0200

    Deploy record id cache

--- late.js
+++ late.js
@@ -2929,7 +2929,7 @@ function reportSzzServiceWorkerError(err){
 function registerSzzServiceWorker(){
   if(!("serviceWorker" in navigator) || !/^https?:$/.test(location.protocol)) return Promise.resolve(null);
   if(window.__szzServiceWorkerRegistrationPromise) return window.__szzServiceWorkerRegistrationPromise;
-  const serviceWorkerBuildVersion="2026-08-11-multi-source-cache-v195";
+  const serviceWorkerBuildVersion="2026-08-11-record-id-cache-v196";
   const activatedKey=`astipSzzSwActivated:${serviceWorkerBuildVersion}`;
   if(!window.__szzSwControllerChangeBound){
     window.__szzSwControllerChangeBound=true;
