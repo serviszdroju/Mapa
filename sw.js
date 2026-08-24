7e63deb9bb386180613c2a5d91ba53ec9aa86b2d
Jan Soldan <jansoldan@Jan--MacBook-Air.local>
2026-08-24 12:26:45 +0200

    Deploy DOM update helper split

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "astip-szz-v426";
+const CACHE_VERSION = "astip-szz-v427";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const TILE_CACHE = "astip-szz-map-tiles-v1";
