c98d5501babe3f3264b0a8677c864c0ce1caf2ec
Jan Soldan <jansoldan@Jan--MacBook-Air.local>
2026-08-24 13:26:26 +0200

    Deploy detail hidden field helper split

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "astip-szz-v434";
+const CACHE_VERSION = "astip-szz-v435";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const TILE_CACHE = "astip-szz-map-tiles-v1";
