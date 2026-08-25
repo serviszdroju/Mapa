efbd0baeee327a797d269803a89e9da65a5cfacf
Jan Soldan <jansoldan@Jan--MacBook-Air.local>
2026-08-25 19:31:13 +0200

    Deploy auth startup guards

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "astip-szz-v495";
+const CACHE_VERSION = "astip-szz-v497";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const TILE_CACHE = "astip-szz-map-tiles-v1";
