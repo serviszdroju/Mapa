c48cb83c6569353e2a9b33d1828c1d1523bda102
Jan Soldan <jan.soldan@astip.cz>
2026-08-31 10:32:36 +0200

    Force yellow ordered detail button

--- src/hosted-login.js
+++ src/hosted-login.js
@@ -9,7 +9,7 @@ import {
 } from "./firebase-auth.js";
 
 const HOSTED_APP_URL="https://serviszdroju.github.io/Mapa/";
-const EMAIL_LOGIN_BUILD_VERSION="stop-handoff-no-legacy-auto-v657";
+const EMAIL_LOGIN_BUILD_VERSION="ordered-button-yellow-v658";
 window.__firebaseConfig=window.__firebaseConfig || firebaseConfig;
 
 const authUiState={
