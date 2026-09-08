99c27f2e1052c7261eea2d53ac9f04ef0558247f
Jan Soldan <jansoldan@Jan--MacBook-Air.local>
2026-09-08 20:29:52 +0200

    Bump Android fast start cache version

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "astip-szz-v680";
+const CACHE_VERSION = "astip-szz-v681";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const TILE_CACHE = "astip-szz-map-tiles-v1";
--- src/hosted-login.js
+++ src/hosted-login.js
@@ -9,7 +9,7 @@ import {
 } from "./firebase-auth.js";
 
 const HOSTED_APP_URL="https://serviszdroju.github.io/Mapa/";
-const EMAIL_LOGIN_BUILD_VERSION="android-fast-start-v680";
+const EMAIL_LOGIN_BUILD_VERSION="android-fast-start-v681";
 window.__firebaseConfig=window.__firebaseConfig || firebaseConfig;
 
 const authUiState={
