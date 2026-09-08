b4e2238fc8058992a26a4524aad42c70fae212f1
Jan Soldan <jansoldan@Jan--MacBook-Air.local>
2026-09-08 13:38:17 +0200

    Fix inline Android auth restore state

--- src/hosted-login.js
+++ src/hosted-login.js
@@ -9,7 +9,7 @@ import {
 } from "./firebase-auth.js";
 
 const HOSTED_APP_URL="https://serviszdroju.github.io/Mapa/";
-const EMAIL_LOGIN_BUILD_VERSION="android-auth-persist-v675";
+const EMAIL_LOGIN_BUILD_VERSION="android-auth-persist-v676";
 window.__firebaseConfig=window.__firebaseConfig || firebaseConfig;
 
 const authUiState={
