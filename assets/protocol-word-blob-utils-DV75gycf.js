bf6509af9b17b00f753c3ccb173b6d8ecfb0bbcb
Jan Soldan <jan.soldan@astip.cz>
2026-09-23 15:39:48 +0200

    Deploy authenticated startup recovery

--- assets/protocol-word-blob-utils-DV75gycf.js
+++
@@ -1,1 +0,0 @@
-import{_ as u}from"./index-CGpWoRSt.js";function c({buildProtocolWordEntries:r}){let o=null;function t(){return o||(o=u(()=>import("./zip-docx-CsnmNYND.js"),[],import.meta.url)),o}async function l(e={}){const{buildDocxBlob:i}=await t();return i(await r(e))}return{buildProtocolWordBlob:l}}export{c as createProtocolWordBlobHelpers};
